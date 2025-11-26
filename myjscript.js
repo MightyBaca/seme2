e7614003505c09aac2bc01e872f94242de169eb0
MightyBaca <samuel.gazi@gmail.com>
2025-11-26 23:16:35 +0100

    Form validation for Kontakt chaged from required to javascript function

--- myjscript.js
+++ myjscript.js
@@ -23,3 +23,38 @@ document.addEventListener('DOMContentLoaded', () => {
     if (e.target.tagName === 'A') sidebar.classList.remove('open');
   });
 });
+
+/*Dropdown list select form */
+var select = document.getElementById("Vyber_Triedy");
+var options = ["Netuším","5a", "5b", "5c", "6a", "6b +"];
+
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+}
+
+
+/*Form validation*/
+function FormValid() {
+  // Get the value of the input field with ID: InName
+  let x = document.getElementById("InName").value;
+  // If x is empty
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  let MF=/\S+@\S+\.\S+/;
+  let y=document.getElementById("InMail").value;
+  if(!(MF.test(y))){
+    alert("Please fill out email with correct form: 'example@example.ex'");
+    return false;
+  }
+  let z = document.getElementById("Mess").value;
+  if (z == "") {
+    alert("Please type in a message");
+    return false;
+  }
+}
